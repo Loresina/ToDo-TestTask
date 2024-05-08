17759c507c7539747489c7daebd2221b5126487c
Loresina <loresina86@gmail.com>
2024-05-08 15:23:52 +0700

    add: ci/cd github actions

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -5,10 +5,14 @@ import App from "./App";
 import appStore from "../store/store";
 // import { renderWithProviders } from "../utils/test-utils";
 
-test("App should have correct initial render", () => {
-  render(<App />, {
-    wrapper: ({ children }) => <Provider store={appStore}>{children}</Provider>,
-  });
+describe("App", () => {
+  test("App should have correct initial render", () => {
+    render(<App />, {
+      wrapper: ({ children }) => (
+        <Provider store={appStore}>{children}</Provider>
+      ),
+    });
 
-  expect(screen.getByText(/todos/i)).toBeInTheDocument();
+    expect(screen.getByText(/todos/i)).toBeInTheDocument();
+  });
 });
